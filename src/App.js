ed66663d1965ab28e176f1f59461a3bc054ac1a9
Poomrapee kanthapong <poomza209@gmail.com>
2020-04-10 01:12:35 +0700

    Revert "update"
    This reverts commit 6bff879912b381243a7f7ff22e80a5f6dc9b6cdd.

--- src/App.js
+++ src/App.js
@@ -1,27 +1,15 @@
 import React from 'react';
-import { StatusBar } from 'react-native';
+import {StatusBar} from 'react-native';
 import Router from './routes';
 import 'react-native-gesture-handler';
-import { Provider as ReduxProvider } from 'react-redux';
-import { Provider as PaperProvider } from 'react-native-paper';
+import {Provider as ReduxProvider} from 'react-redux';
+import {Provider as PaperProvider} from 'react-native-paper';
 import reduxStore from './store';
-import { SafeAreaProvider } from 'react-native-safe-area-context';
+import {SafeAreaProvider} from 'react-native-safe-area-context';
 import paperTheme from './themes/paper';
-import { PersistGate } from 'redux-persist/integration/react';
-import * as eva from '@eva-design/eva';
-import { ApplicationProvider, IconRegistry } from '@ui-kitten/components';
-import { EvaIconsPack } from '@ui-kitten/eva-icons';
-import { ThemeContext } from './themes/theme-context';
+import {PersistGate} from 'redux-persist/integration/react';
 
 export default () => {
-
-  const [theme, setTheme] = React.useState('light');
-
-  const toggleTheme = () => {
-    const nextTheme = theme === 'light' ? 'dark' : 'light';
-    setTheme(nextTheme);
-  };
-
   return (
     <ReduxProvider store={reduxStore.store}>
       <PersistGate loading={null} persistor={reduxStore.persistor}>
@@ -31,14 +19,9 @@ export default () => {
             backgroundColor="transparent"
             barStyle="dark-content"
           /> */}
-        <IconRegistry icons={EvaIconsPack} />
-        <ThemeContext.Provider value={{ theme, toggleTheme }}>
-          <ApplicationProvider {...eva} theme={eva[theme]}>
-            <SafeAreaProvider>
-              <Router />
-            </SafeAreaProvider>
-          </ApplicationProvider>
-        </ThemeContext.Provider>
+          <SafeAreaProvider>
+            <Router />
+          </SafeAreaProvider>
         {/* </PaperProvider> */}
       </PersistGate>
     </ReduxProvider>
--- src/screens/Main/BottomTab/AllStore/styles.js
+++ src/screens/Main/BottomTab/AllStore/styles.js
@@ -1,8 +1,5 @@
 import { StyleSheet } from "react-native";
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        backgroundColor: '#ecf0f1',
-      },
+    
 });
